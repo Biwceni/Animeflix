e670c69c2eca5fa6c7d1ffe02e2a6994bf5bfcd5
Biwceni <fabio.ormeni@hotmail.com>
2022-03-24 15:55:54 -0300

    Site Animeflix

---
+++ js/owl/setup.js
@@ -0,0 +1,276 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function abrir1() {
+    document.getElementById("trocar").style.opacity = "1";
+    document.getElementById("trocar").style.pointerEvents = "auto";
+    document.getElementById("trocar").style.backgroundImage = " url('./img/anime1.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir2() {
+    document.getElementById("trocar2").style.opacity = "1";
+    document.getElementById("trocar2").style.pointerEvents = "auto";
+    document.getElementById("trocar2").style.backgroundImage = " url('./img/anime2.jpg') ";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar2").style.zIndex = "9999";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir3() {
+    document.getElementById("trocar3").style.opacity = "1";
+    document.getElementById("trocar3").style.pointerEvents = "auto";
+    document.getElementById("trocar3").style.backgroundImage = " url('./img/anime3.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar3").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir4() {
+    document.getElementById("trocar4").style.opacity = "1";
+    document.getElementById("trocar4").style.pointerEvents = "auto";
+    document.getElementById("trocar4").style.backgroundImage = " url('./img/anime4.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar4").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir5() {
+    document.getElementById("trocar5").style.opacity = "1";
+    document.getElementById("trocar5").style.pointerEvents = "auto";
+    document.getElementById("trocar5").style.backgroundImage = " url('./img/anime5.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar5").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir6() {
+    document.getElementById("trocar6").style.opacity = "1";
+    document.getElementById("trocar6").style.pointerEvents = "auto";
+    document.getElementById("trocar6").style.backgroundImage = " url('./img/anime6.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar6").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir7() {
+    document.getElementById("trocar7").style.opacity = "1";
+    document.getElementById("trocar7").style.pointerEvents = "auto";
+    document.getElementById("trocar7").style.backgroundImage = " url('./img/anime7.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar7").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir8() {
+    document.getElementById("trocar8").style.opacity = "1";
+    document.getElementById("trocar8").style.pointerEvents = "auto";
+    document.getElementById("trocar8").style.backgroundImage = " url('./img/anime8.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar8").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir9() {
+    document.getElementById("trocar9").style.opacity = "1";
+    document.getElementById("trocar9").style.pointerEvents = "auto";
+    document.getElementById("trocar9").style.backgroundImage = " url('./img/anime9.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("trocar10").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar9").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+    document.getElementById("trocar10").style.zIndex = "0";
+}
+
+function abrir10() {
+    document.getElementById("trocar10").style.opacity = "1";
+    document.getElementById("trocar10").style.pointerEvents = "auto";
+    document.getElementById("trocar10").style.backgroundImage = " url('./img/anime10.jpg') ";
+    document.getElementById("trocar2").style.opacity = "0";
+    document.getElementById("trocar3").style.opacity = "0";
+    document.getElementById("trocar4").style.opacity = "0";
+    document.getElementById("trocar5").style.opacity = "0";
+    document.getElementById("trocar6").style.opacity = "0";
+    document.getElementById("trocar7").style.opacity = "0";
+    document.getElementById("trocar8").style.opacity = "0";
+    document.getElementById("trocar9").style.opacity = "0";
+    document.getElementById("trocar").style.opacity = "0";
+    document.getElementById("intro").style.opacity = "0";
+    document.getElementById("trocar10").style.zIndex = "9999";
+    document.getElementById("trocar2").style.zIndex = "0";
+    document.getElementById("trocar3").style.zIndex = "0";
+    document.getElementById("trocar4").style.zIndex = "0";
+    document.getElementById("trocar5").style.zIndex = "0";
+    document.getElementById("trocar6").style.zIndex = "0";
+    document.getElementById("trocar7").style.zIndex = "0";
+    document.getElementById("trocar8").style.zIndex = "0";
+    document.getElementById("trocar9").style.zIndex = "0";
+    document.getElementById("trocar").style.zIndex = "0";
+}
